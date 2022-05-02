20b270e260064e802ef369ae604791738cc0c7b5
rolandoChapa <roly3412@gmail.com>
2022-05-02 18:00:13 -0500

    adding dist; uploading the app; take 1

--- src/navBarNBA.jsx
+++
@@ -1,7 +0,0 @@
-export default function NavBarNBA() {
-return (
-    <div className="flex flex-row flex-wrap justify-center gap-4 lg:gap-12 py-6 text-4xl bg-gradient-to-r from-gray-100 to-gray-300 tracking-tighter">
-        <a className="z-10 font-plusjak font-bold text-black">NBA Stars 🌟</a>
-    </div>
-)
-}
